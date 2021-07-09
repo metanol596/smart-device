5b3759a5b9136ada1394345e6ef1c65506be1b64
Astemir <amaremkulov96@gmail.com>
2021-07-09 23:13:39 +0300

    Добавляет вёрстку подвала

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,19 @@
 "use strict";
 
+const accordionItems = document.querySelectorAll('.page-footer__accordion-item');
+accordionItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const activeAccordionItem = document.querySelector('.page-footer__accordion-item.active');
+
+    if (activeAccordionItem && activeAccordionItem !== item) {
+      activeAccordionItem.classList.toggle('active');
+    }
+
+    item.classList.toggle('active');
+  });
+});
+"use strict";
+
 const MOBILE_ONLY_WIDTH = 767;
 const CONSULTATION_BUTTON_TEXT = 'Получить бесплатную консультацию';
 const CONSULTATION_BUTTON_TEXT_MOBILE = 'Бесплатная консультация';
---
+++ source/js/accordion.js
@@ -0,0 +1,13 @@
+const accordionItems = document.querySelectorAll('.page-footer__accordion-item');
+
+accordionItems.forEach((item) => {
+	item.addEventListener('click', () => {
+		const activeAccordionItem = document.querySelector('.page-footer__accordion-item.active');
+
+			if (activeAccordionItem && activeAccordionItem !== item) {
+				activeAccordionItem.classList.toggle('active');
+			}
+
+		item.classList.toggle('active');
+	})
+})
