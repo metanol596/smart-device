a5fa0584d3569943e79e37c65c7677ea0e4fc005
Astemir <amaremkulov96@gmail.com>
2021-07-11 18:23:22 +0300

    Добавляет вёрстку блока Преимущества

--- build/js/main.js
+++ build/js/main.js
@@ -12,15 +12,3 @@ accordionItems.forEach(item => {
     item.classList.toggle('active');
   });
 });
-"use strict";
-
-const MOBILE_ONLY_WIDTH = 767;
-const CONSULTATION_BUTTON_TEXT = 'Получить бесплатную консультацию';
-const CONSULTATION_BUTTON_TEXT_MOBILE = 'Бесплатная консультация';
-const consultaionButton = document.querySelector('.promo a');
-
-if (document.documentElement.clientWidth <= MOBILE_ONLY_WIDTH) {
-  consultaionButton.textContent = CONSULTATION_BUTTON_TEXT_MOBILE;
-} else {
-  consultaionButton.textContent = CONSULTATION_BUTTON_TEXT;
-}
