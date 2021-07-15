5a1195c04dd751d821b43b9af4b3d6cfef1bf996
Astemir <amaremkulov96@gmail.com>
2021-07-15 19:14:00 +0300

    Исправления

--- build/js/main.js
+++ build/js/main.js
@@ -2,6 +2,9 @@
 
 const accordionItems = document.querySelectorAll('.page-footer__accordion-item');
 accordionItems.forEach(item => {
+  item.classList.remove('no-js');
+});
+accordionItems.forEach(item => {
   item.addEventListener('click', () => {
     const activeAccordionItem = document.querySelector('.page-footer__accordion-item.active');
 
--- source/js/accordion.js
+++ source/js/accordion.js
@@ -1,6 +1,10 @@
 const accordionItems = document.querySelectorAll('.page-footer__accordion-item');
 
 accordionItems.forEach((item) => {
+	item.classList.remove('no-js');
+})
+
+accordionItems.forEach((item) => {
 	item.addEventListener('click', () => {
 		const activeAccordionItem = document.querySelector('.page-footer__accordion-item.active');
 
